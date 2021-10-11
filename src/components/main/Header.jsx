c9fb3c322ec86cdef1580d04dec6d767bc4a851c
GabrielSER <gs.serna10@uniandes.edu.co>
2021-10-11 18:44:54 -0500

    clsx

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -1,7 +1,9 @@
+
 import Logo from '../ui/Logo'
 import { useState } from 'react'
 import Input from '../ui/Input'
 import Button from '../ui/Button'
+import clsx from 'clsx'
 
 const initialSearch = {
     searchb: ''
@@ -21,10 +23,42 @@ const Header = () => {
     }
 
     return (
-        <div className='flex p-4 flex-shrink-0 w-full h-24 md:h-36 bg-gold shadow-lg'>
-            <div className='flex gap-2 md:gap-4 w-full h-full items-center'>
-                <Logo className='object-contain h-full md:hidden' />
-                <span className='hidden md:flex flex-wrap md:gap-x-2 text-gray-800 font-moontime text-xl md:text-4xl font-black  '>
+        <div
+            className={clsx(
+                'flex',
+                'flex-shrink-0',
+                'w-full',
+                'h-24 md:h-36',
+                'p-4',
+                'bg-gold',
+                'shadow-lg'
+            )}
+        >
+            <div
+                className={clsx(
+                    'flex',
+                    'w-full h-full',
+                    'gap-2 md:gap-4',
+                    'items-center'
+                )}
+            >
+                <Logo
+                    className={clsx(
+                        'object-contain',
+                        'h-full',
+                        'md:hidden'
+                    )}
+                />
+                <span
+                    className={clsx(
+                        'hidden md:flex',
+                        'flex-wrap',
+                        'md:gap-x-2',
+                        'text-2xl lg:text-4xl',
+                        'text-gray-800',
+                        'font-moontime font-black',
+                    )}
+                >
                     La belleza de estar en
                     <span className='text-secondary'>
                         ARMOONIA
@@ -32,9 +66,24 @@ const Header = () => {
                     con lo que eres
                 </span>
                 <div class="flex md:p-52">
-                    <div className='container h-36 flex justify-around items-center'>
+                    <div
+                        className={clsx(
+                            'container',
+                            'flex',
+                            'justify-around items-center',
+                            'h-36'
+                        )}
+                    >
                         <div className='flex w-10 md:p-6 md:justify-evenly'>
-                            <div className="absolute top-4 left-3"> <i class="fa fa-search text-gray-400 md:z-20 hover:text-gray-500"></i> </div>
+                            <div className="absolute top-4 left-3">
+                                <i className={clsx(
+                                    'fa fa-search',
+                                    'text-gray-400',
+                                    'md:z-20',
+                                    'hover:text-gray-500'
+                                )}
+                                />
+                            </div>
                             <Input
                                 id='search'
                                 value={state.searchb}
@@ -47,7 +96,7 @@ const Header = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
